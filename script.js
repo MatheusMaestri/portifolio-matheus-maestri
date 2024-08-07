879afb9c6b52d939363c135f9116433b9c4717e3
MatheusMaestri <matheusmaestri28@gmail.com>
2024-08-07 20:45:43 -0300

    Alterando CSS

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ const myObserver = new IntersectionObserver( (entrou) =>{
 })
 
 elementos.forEach( (elementos) => myObserver.observe(elementos))
+
+// NO CÓDIGO ABAIXO FAZ O EFEITO DE ESCRITA
+
+document.addEventListener("DOMContentLoaded", function() {
+    const spanElement = document.getElementById('animated-span');
+    const text = 'Front-End';
+    let index = 0;
+    let isDeleting = false;
+    let delayAfterTyping = 3000; 
+
+    function typeWriter() {
+        if (isDeleting) {
+            spanElement.innerHTML = text.substring(0, index - 1);
+            index--;
+            if (index === 1) {
+                isDeleting = false;
+            }
+        } else {
+            spanElement.innerHTML = text.substring(0, index + 1);
+            index++;
+            if (index === text.length) {
+                setTimeout(() => {
+                    isDeleting = true;
+                }, delayAfterTyping);
+            }
+        }
+        setTimeout(typeWriter, isDeleting ? 100 : 200);
+    }
+
+    typeWriter();
+});
